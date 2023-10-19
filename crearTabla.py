2ee543f4f1ecc4ee7d2b0dc62a8481c8c418439f
SYDAW <sdru1203@iesmartinezm.es>
2023-10-19 14:26:18 +0200

    Tercer commit

--- crearTabla.py
+++ crearTabla.py
@@ -1,16 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .djangoForms import formTabla
 
-def crear_tabla (pedir):
+def crear_tabla (request):
     if request.method == "POST":
         form = formTabla(request.POST)
-        if from.es_valido():
+        if form.es_valido():
             filas = form.cleaned_data["filas"]
             columnas = form.cleaned_data["columnas"]
 
             return redirect("tableroCreado", filas=filas, columnas=columnas)
 
     else:
-        from = formTabla()
+        form = formTabla()
 
     return render(request, "index.html", {"form": form})
--- enseñarTablero.py
+++ enseñarTablero.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
 def enseñarTablero(request, filas, columnas):
-    return render(request, "tableroCreado.html", {"filas": filas, "columnas": columnas, "datosTabla": datosTabla})
+    tabla_datos = []
+    for i in range(filas):
+        fila = []
+        for j in range(columnas):
+            celda = f"Fila {i}, Columna {j}"
+            fila.append(celda)
+        tabla_datos.append(fila)
+        
+    return render(request, "tableroCreado.html", {"filas": filas, "columnas": columnas, "tabla_datos": tabla_datos})
